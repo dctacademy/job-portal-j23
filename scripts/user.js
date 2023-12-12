77ea58a44a2f0141b3f8dd3567fc3d9393c16f23
Aniruddha Sg <aniruddhasg@Aniruddhas-MacBook-Pro.local>
2023-12-12 14:19:17 +0530

    with disconnect method

--- scripts/user.js
+++ scripts/user.js
@@ -12,13 +12,15 @@ const runScript = async () => {
             password: "secret123", 
             role: "admin" 
         })
-        const checkUser = User.findOne({ email: user.email })
+        const checkUser = await User.findOne({ email: user.email })
         if(checkUser) {
             console.log('account already created')
         } else {
             await user.save()
             console.log('user created', user)
         }
+        await db.disconnect()
+        console.log('db connection closed')
     } catch(e) {
         console.log('error connecting to db',e)
     }
